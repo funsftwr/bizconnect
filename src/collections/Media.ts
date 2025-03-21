49c7a87ba3d324fbd7a454853fb486d8c57475e5
tuanha <toni.tuanha@peer39.com>
2025-03-21 18:53:04 +0700

    Adjust Media reshap sizes

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -45,34 +45,11 @@ export const Media: CollectionConfig = {
     imageSizes: [
       {
         name: 'thumbnail',
-        width: 300,
-      },
-      {
-        name: 'square',
-        width: 500,
-        height: 500,
-      },
-      {
-        name: 'small',
-        width: 600,
-      },
-      {
-        name: 'medium',
-        width: 900,
-      },
-      {
-        name: 'large',
-        width: 1400,
-      },
-      {
-        name: 'xlarge',
-        width: 1920,
+        height: 240,
       },
       {
         name: 'og',
-        width: 1200,
-        height: 630,
-        crop: 'center',
+        height: 720,
       },
     ],
   },
